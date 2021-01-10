07d60ac2d1cd61e54b632124d0fede813d5d19fe
Ariana Winters <arianaw15@gmail.com>
2021-01-09 23:45:40 -0700

    Simplified code, but still seeing 'undefined' for answer.text

--- script.js
+++ script.js
@@ -4,53 +4,54 @@ var startBtn = document.getElementById('start-btn')
 var questionContainer = document.getElementById('question-container')
 var nextBtn = document.getElementById('next-btn')
 var answerButtonsElement = document.getElementById('answer-buttons')
-var question = document.getElementById('question')
+var questions = document.getElementById('question')
 var questionName = [{
-        question: "Question 1",
-        answer: [
-            {text: 'answer1.1', correct: false},
-            {text: 'answer1.2', correct: true},
-            {text: 'answer1.3', correct: false},
-            {text: 'answer1.4', correct: false}
-        ]
-    },
-    {question: "Question 2",
-answer: [
-    {text: 'answer2.1', correct: false},
-            {text: 'answer2.2', correct: false},
-            {text: 'answer2.3', correct: false},
-            {text: 'answer2.4', correct: true}
-]}
-]
+    question: "Question 1",
+    answer: [{
+            text: 'answer1.1', correct: false
+        },
+        {
+            text: 'answer1.2', correct: false
+        },
+        {
+            text: 'answer1.3', correct: true
+        },
+        {
+            text: 'answer1.4', correct: false
+        }
+    ]
+}]
 
 
 
+startBtn.addEventListener("click", (startQuiz))
 
-// When a user clicks "start" the first question and answer options appear
-startBtn.addEventListener('click', startQuiz)
-
-function startQuiz(){
-startBtn.classList.add('hide');
-questionContainer.classList.remove('hide');
-resetState();
-for (let i = 0; i < questionName.length; i++) {
-    document.getElementById("question").innerHTML = questionName[i].question;
-    questionName[i].answer.forEach(answer => {
-        const answerBtn = document.createElement('button');
-        answerBtn.innerHTML = answer.text;
-        answerBtn.classList.add('btn');
-        answerBtn.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(answerBtn);
-    })
-    
-}
-    
+function startQuiz() {
+    startBtn.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    resetState()
+    setQuestion()
 }
 
+function setQuestion(){
+    for (let index = 0; index < questionName.length; index++) {
+        console.log(questionName[index].question);
+        questions.innerHTML = questionName[index].question;
+        console.log(questionName[index].answer)
+        questionName[index].answer.forEach(() => {
+            var answerBtn = document.createElement('button')
+            answerBtn.innerHTML = questionName[index].answer.text
+            answerBtn.classList.add('btn')
+            answerBtn.addEventListener('click', selectAnswer)
+            answerButtonsElement.appendChild(answerBtn);
+            console.log(answerBtn)
+        })
+    }
+}
 //Function to reset the state to allow for the next set of questions
-function resetState(){
+function resetState() {
     nextBtn.classList.add('hide')
-    while (answerButtonsElement.firstChild){
+    while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
@@ -59,22 +60,18 @@ function resetState(){
 
 function selectAnswer() {
     nextBtn.classList.remove('hide');
-    if (correct = "true"){
+    if (correct = "true") {
         alert("That's correct!")
-    }
-    else{
+    } else {
         alert("That's incorrect")
     }
 }
-nextBtn.addEventListener('click', () =>{
+nextBtn.addEventListener('click', () => {
     setNextQuestion()
 })
 
 function setNextQuestion() {
-    nextBtn.classList.add('hide'); 
-    resetState(); 
+    nextBtn.classList.add('hide');
+    resetState();
+    questionName[index++]
 }
-
-
-
-//When the user answers all of the questions, their score will be displayed at the end
